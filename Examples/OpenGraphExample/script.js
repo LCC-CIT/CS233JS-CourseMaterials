604d57e3c37b81690f98b75df9bf3ac79bb1cc91
Brian Bird <ProfBird@users.noreply.github.com>
2026-02-18 09:27:38 -0800

    OpenGraph example Refactored to use async / await

--- Examples/OpenGraphExample/script.js
+++ Examples/OpenGraphExample/script.js
@@ -3,33 +3,31 @@
 // Fetch OpenGraph data from a URL when the form is submitted
 document.getElementById('fetchForm').addEventListener('submit', getMetadata);
 
-    function getMetadata(event) {
+async function getMetadata(event) {
     event.preventDefault();
     const SITE_URL = encodeURIComponent(event.target.elements.url.value); // Encode the URL entered by the user to handle special characters
     const API_URL = "https://api.linkpreview.net/";
     const API_KEY = "fd3f9a0c9fafe271af5bd823cf3f0c02"; // TODO: hide the API key
     // This is a free API key, but you should get your own
 
+    try {
+        // Fetch the metadata from the linkpreview API
+        const response = await fetch(`${API_URL}?q=${SITE_URL}&key=${API_KEY}`);
 
-    // Fetch the metadata from the linkpreview API
-    fetch(`${API_URL}?q=${SITE_URL}&key=${API_KEY}`)
-    // Check the response and throw an error if it's not
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json(); // return the JSON data in a promise to the next then block
-        })
+        // Check the response and throw an error if it's not ok
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
         // Parse the JSON and display the OpenGraph data on the page
-        .then(data => {
-            writeDataToPage(data);
-        })
-        // Catch any errors and log them to the console
-        .catch(error => {
-            console.error('Error fetching OpenGraph data:', error);
-            alert("There was an error fetching the OpenGraph data. Please try again.");
-        });
-};
+        const data = await response.json();
+        writeDataToPage(data);
+    } catch (error) {
+        // Log the error to the console and alert the user
+        console.error('Error fetching OpenGraph data:', error);
+        alert("There was an error fetching the OpenGraph data. Please try again.");
+    }
+}
 
 // Write the web site metadata to the page
 function writeDataToPage(data) {
