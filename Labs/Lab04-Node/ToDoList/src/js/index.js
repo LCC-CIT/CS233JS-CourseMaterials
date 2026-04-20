e276758f6561f8b2e15cd62a89072e4e8c1144a6
Brian Bird <ProfBird@users.noreply.github.com>
2026-04-20 11:41:30 -0700

    Fixed vite issues in the ToDoList for lab 4

--- Labs/Lab04-Node/ToDoList/src/js/index.js
+++ Labs/Lab04-Node/ToDoList/src/js/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../css/styles.css';
 import { TaskModel } from './model.js';
 import { TaskView } from './view.js';
 import { TaskController } from './controller.js';
